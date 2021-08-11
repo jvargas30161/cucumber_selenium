f5810c33b0a579ac828ba4e6916aec98aabfd495
jvargas30161 <jvargas30161@gmail.com>
2021-08-11 23:53:29 +0200

    update "Ingreso a página www.Imalittletester.com" Hooks_Before_After

--- src/test/java/features/test.feature
+++ src/test/java/features/test.feature
@@ -5,3 +5,8 @@ Feature: Categoria Comics
     Given   El usuario se encuentra en la pagina home de imalittletester
     When    Hace clic sobre el boton The litlle tester comics
     Then    Se debe redirigir a la pantalla comics
+
+  Scenario: Ingresar a la catagoría comics desde la pantalla home.
+    Given   El usuario se encuentra en la pagina home de imalittletester
+    When    Hace clic sobre el boton The litlle tester comics
+    Then    Se debe redirigir a la pantalla comics
--- src/test/java/seleniumgluecode/Hooks.java
+++ src/test/java/seleniumgluecode/Hooks.java
@@ -7,12 +7,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Hooks {
 
     private static ChromeDriver driver;
+    private static int numberofCase = 0;
+
 
     @Before
 
     //Vamos a crear métodos 1 que se ejecute antes de cada esceario y uno segundo se ejecutara despues.
 
     public void setUp(){
+        numberofCase ++;
+        System.out.println("Se esta ejecutando el escenario nro: " + numberofCase);
         System.setProperty("webdriver.chrome.driver","./src/test/resources/chromedriver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://imalittletester.com/");
@@ -22,6 +26,7 @@ public class Hooks {
 
     @After
     public void tearDown(){
+        System.out.println("El escenario nro: " + numberofCase + " se ejecuto correctamente. ");
         driver.quit();
 
     }
